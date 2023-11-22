8739cd2e7082445c8451890da76e835e7a501bf7
Adolfo Blanco <ajb156@outlook.com>
2023-11-22 11:29:52 +0100

    enviroment

--- frontend/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiAuthUrl: 'http://localhost:4000/api/v1',
-  apiUrl: 'http://localhost:4000/api/v1',
-  wsUrl: 'http://localhost:8080',
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  apiAuthUrl: 'http://localhost:4000/api/v1',
-  apiUrl: 'http://localhost:4000/api/v1',
-  wsUrl: 'http://localhost:8080',
-};
